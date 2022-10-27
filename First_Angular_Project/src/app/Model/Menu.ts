085cdb8c5f624bb1fcd7963bb608502b7f85e901
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-10-27 22:40:36 +0530

    Added menu.ts in model package

---
+++ First_Angular_Project/src/app/Model/Menu.ts
@@ -0,0 +1,11 @@
+export class Menu {
+    category: string;
+    name:string;
+    price:number;
+    constructor(){
+        this.category= ""
+        this.name=""
+        this.price =0
+    }
+}
+
--- First_Angular_Project/src/app/app.component.ts
+++ First_Angular_Project/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'First_Angular_Project';
+
+  
 }
